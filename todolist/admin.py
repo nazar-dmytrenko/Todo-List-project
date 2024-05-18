1ca143998a8920bf1c6e034f88f9fad19f39c18a
Nazar Dmytrenko <nazarkadm@gmail.com>
2024-05-18 17:19:35 +0300

    Add our new models in admin panel

--- todolist/admin.py
+++ todolist/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from todolist.models import Task, Tag
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["content", "created_at", "deadline", "is_done"]
+    list_filter = ["content", "created_at"]
+    search_fields = ["content", "tags__name"]
+
+
+admin.site.register(Tag)
