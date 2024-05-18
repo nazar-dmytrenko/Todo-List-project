5efb3bf9a365bc07b79136b1a6b7876e2307066d
Nazar Dmytrenko <nazarkadm@gmail.com>
2024-05-18 18:15:22 +0300

    Add new urls in urls.py

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,65 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todolist.views import (
+    TaskListView,
+    TaskCreateView,
+    TaskDetailView,
+    TaskUpdateView,
+    TaskDeleteView,
+    TaskCompleteView,
+    TaskUndoView,
+
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", TaskListView.as_view(), name="index"),
+    path(
+        "create/",
+        TaskCreateView.as_view(),
+        name="task-create"),
+    path(
+        "<int:pk>/",
+        TaskDetailView.as_view(),
+        name="task-detail"),
+    path(
+        "<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task-update"),
+    path(
+        "<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task-delete"),
+    path(
+        "<int:pk>/complete/",
+        TaskCompleteView.as_view(),
+        name="task-complete"),
+    path(
+        "<int:pk>/undo/",
+        TaskUndoView.as_view(),
+        name="task-undo"),
+
+    path(
+        "tags/",
+        TagListView.as_view(),
+        name="tag-list"),
+    path(
+        "tags/create/",
+        TagCreateView.as_view(),
+        name="tag-create"),
+    path(
+        "tags/<int:pk>/update/",
+        TagUpdateView.as_view(),
+        name="tag-update"),
+    path(
+        "tags/<int:pk>/delete/",
+        TagDeleteView.as_view(),
+        name="tag-delete"),
 ]
+
+app_name = "todolist"
